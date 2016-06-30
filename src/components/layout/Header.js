ca663efad96409bf1bc937ef81b0ada755f5a69d
thisgirlsaphony <emily@thisgirlsaphony.com>
2016-06-30 06:54:34 -0500

    style tweek

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -4,7 +4,7 @@ import { IndexLink } from 'react-router';
 const Header  = () => {
   return (
     <nav className="navbar navbar-default hover-display">
-      <div className="container-fluid">
+      <div className="container-fluid" style={{visible:'hidden'}}>
         <div className="navbar-header">
           <IndexLink className="navbar-brand" to="/"><i className="fa fa-bookmark-o" /> &nbsp; Locations</IndexLink>
         </div>
